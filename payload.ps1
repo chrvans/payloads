2e94e8f27277520742e8cd82183e0d04a9a25d6d
chrvans <128791045+chrvans@users.noreply.github.com>
2023-04-22 08:56:10 +0200

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,5 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$WebClient = New-Object System.Net.WebClient
+$output = "C:\Users\$env:USERNAME\Desktop\bigup.jpg"
 wget "https://www.justgeek.fr/wp-content/uploads/2023/01/mettre-fond-ecran-anime-pc-768x432.jpg" -outFile "C:\Users\$env:USERNAME\Desktop\bigup.jpg"
 $setwallpapersrc = @"
 using System.Runtime.InteropServices;
